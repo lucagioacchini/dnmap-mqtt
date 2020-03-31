4117f9b40a69a86913f3c40e5ec0c15826af7f58
luca <lucagioack@gmail.com>
2020-03-31 22:30:54 +0200

    protocol fixed with retransmission

--- client.py
+++ client.py
@@ -5,6 +5,8 @@ import json
 import uuid  
 from threading import Thread
 
+DEBUG = 0
+
 CL_ID = uuid.uuid1().hex
 MQTT_BROKER = 'broker.hivemq.com'
 MQTT_PORT = 1883
@@ -12,21 +14,17 @@ MQTT_PORT = 1883
 QUEUE = {}
 
 def sendAck(mqtt):
+    if DEBUG == 1:
+        print('Sending CL_HELLO')
     msg = json.dumps({
         'id':CL_ID,
         'msg':'CL_HELLO'
     })
     mqtt.publish('dnmap/hello', msg)
 
-def sendDataReq(mqtt):
-    mqtt.done = True
-    msg = json.dumps({
-        'id':CL_ID,
-        'msg':'DATA_REQ'
-    })
-    mqtt.publish(f'dnmap/out/{CL_ID}', msg)
-
 def sendData(mqtt):
+    if DEBUG == 1:
+        print('Sending Data')
     msg = json.dumps({
         'id':CL_ID,
         'msg':mqtt.res
@@ -34,13 +32,36 @@ def sendData(mqtt):
     mqtt.publish(f'dnmap/out/{CL_ID}', msg)
 
 def scanner(mqtt, msg):
+    # If the client has been down, the packet has been dropped, but
+    # now it is up again, change the once_down status
+    if mqtt.connected and mqtt.once_down:
+        mqtt.once_down = False
+    if DEBUG == 1:
+        print('Performing the scanning')
     mqtt.res = nmap.PortScanner().scan(msg['msg'])
-    sendDataReq(mqtt)
-    time.sleep(20)
+    # If the client is connected and it has not been down during
+    # the scanning, send data
+    if mqtt.connected or not mqtt.once_down:
+        if DEBUG == 1:
+            print('Sending Data to the server')
+        sendData(mqtt)
+        mqtt.sent = True
+    # Otherwise drop the packets
+    else:
+        if DEBUG == 1:
+            print('Client down during scanning. Dropping Packet')
 
 class Mqtt():
     def __init__(self, clientID):
-        self.done = False
+        self.connected = False
+        # Set True the following if the client is 
+        # down while scanning 
+        self.once_down = False 
+        
+        # Retransmission stuff
+        self.serv_down = 0
+        self.sent = False
+
         self.clientID = clientID
         self.cli = paho_mqtt.Client(self.clientID) 
         self.res = None
@@ -54,7 +75,7 @@ class Mqtt():
         self.start()
 
     def start(self):
-        self.cli.connect(MQTT_BROKER, MQTT_PORT)
+        self.cli.connect(MQTT_BROKER, MQTT_PORT, keepalive=2)
         self.cli.loop_start()
 
     def stop (self):
@@ -68,6 +89,7 @@ class Mqtt():
         self.cli.subscribe(topic)
 
     def onConnect(self, client, userdata, flags, rc):
+        self.connected = True
         print("Client connected")
         self.subscribe('dnmap/hello')
         self.subscribe(f'dnmap/cmd/{CL_ID}')
@@ -80,8 +102,17 @@ class Mqtt():
 
         # Notify the cliet activeness
         if 'hello' in topic and 'CL' not in msg['msg']:
+            if DEBUG == 1:
+                print('S_HELLO received')
             sendAck(self)
-        
+            if self.sent:
+                if self.serv_down >=3:
+                    if DEBUG == 1:
+                        print('Server was down. Retransmitting data')
+                    sendData(self)
+                else:
+                    self.serv_down += 1
+                
         # When the server sends the nmap command to perform, the 
         # client runs it and sends back the DATA_REQ when it has
         # finished
@@ -89,24 +120,26 @@ class Mqtt():
             print(f"Scanning network: {msg['msg']}")
             t = Thread(target=scanner, args=(self, msg))
             t.start()
-
-        # Receive DATA_REQ_ACK and send the output
-        elif 'out' in topic and msg['msg'] == 'DATA_REQ_ACK':
-            sendData(self)
         
         # When the DATA_ACK is received, the client waits for 
         # new instructions
         elif 'out' in topic and msg['msg'] == 'DATA_ACK':
-            self.done = False
+            self.sent = False
+            if DEBUG == 1:
+                print('Waiting for new commands')
 
     def onDisconnect(self, client, userdata, rc):
+        self.connected = False
+        self.once_down = True
         print('Client disconnected')
 
     def onSubscribe(self, client, userdata, mid, granted_qos):
-        print("Client subscribed")
+        if DEBUG == 1:
+            print("Client subscribed")
     
     def onPublish(self, client, userdata, mid):
-        pass
+        if DEBUG == 1:
+            print('Message published')
 
 
 # Start the server
